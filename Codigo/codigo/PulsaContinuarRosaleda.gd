061f0ef2449bf4f206ec2fd9f5909678217cc2ee
Jearpo <jesusarizajrj@gmail.com>
2024-01-28 11:43:43 +0100

    Sonidos y arreglo de whatsapp

--- Codigo/codigo/PulsaContinuarRosaleda.gd
+++ Codigo/codigo/PulsaContinuarRosaleda.gd
@@ -10,4 +10,3 @@ func _process(delta):
 	await get_tree().create_timer(0.5).timeout
 	if(Input.is_action_pressed("continue")):
 		get_tree().change_scene_to_file("res://escenas/historia/meme_whatsapp.tscn")
-		DisplayServer.window_set_size(1000)
--- Codigo/codigo/PulsaContinuarWhats2.gd
+++ Codigo/codigo/PulsaContinuarWhats2.gd
@@ -5,9 +5,8 @@ extends Label
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	await get_tree().create_timer(0.5).timeout
 	if(Input.is_action_pressed("continue")):
-		get_tree().change_scene_to_file("res://escenas/historia/meme_whatsapp_2.tscn")
+		get_tree().change_scene_to_file("res://escenas/historia/ratadona_cartel.tscn")
