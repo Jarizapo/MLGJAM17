5701e2542b969fd6e0773d93bb3a993cca91f8fe
Jearpo <jesusarizajrj@gmail.com>
2024-01-28 12:38:21 +0100

    Textos añadidos

--- Codigo/codigo/PulsaContinuarWhatsapp2.gd
+++ Codigo/codigo/PulsaContinuarWhatsapp2.gd
@@ -1,5 +1,6 @@
 extends Label
 
+@onready var escena = "res://escenas/historia/meme_whatsapp2.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,4 @@ func _ready():
 func _process(delta):
 	await get_tree().create_timer(0.5).timeout
 	if(Input.is_action_pressed("continue")):
-		get_tree().change_scene_to_file("res://escenas/historia/meme_whatsapp2.tscn")
+		get_tree().change_scene_to_file(escena)
--- Codigo/codigo/boton_jugar.gd
+++ Codigo/codigo/boton_jugar.gd
@@ -1,5 +1,6 @@
 extends TextureButton
 
+@onready var escena = "res://escenas/historia/oveja_rosaleda.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(button_pressed):
-		get_tree().change_scene_to_file("res://escenas/historia/oveja_rosaleda.tscn")
+		get_tree().change_scene_to_file(escena)
